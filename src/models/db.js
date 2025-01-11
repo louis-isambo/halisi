305f9d3279e01e595e880acf77c19afbbc23a346
louis-isambo <leisisambo@gmail.com>
2025-01-11 14:33:31 +0100

    fix bb

--- src/models/db.js
+++ src/models/db.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 // Option 2: Passing parameters separately (sqlite)
 
-const DB  =(function(){
+const DB  = (function(){
 
     if(process.env.run == "dev"){
      return new Sequelize({
@@ -14,20 +14,17 @@ const DB  =(function(){
     }
     if(process.env.run == "production"){
       
-  return  new Sequelize(
-    process.env.DB_NAME,     // Nom de la base de données
-    process.env.DB_USER,     // Utilisateur de la base de données
-    process.env.DB_PASSWORD, // Mot de passe
-    {
-      host: process.env.DB_HOST,  // Hôte
-      port: process.env.DB_PORT,  // Port
-      dialect: 'postgres',        // Type de base de données
-      logging: false,             // Désactiver les logs en production
-    }
-);
-
-module.exports = sequelize;
-
+      return  new Sequelize(
+        process.env.DB_NAME,     // Nom de la base de données
+        process.env.DB_USER,     // Utilisateur de la base de données
+        process.env.DB_PASSWORD, // Mot de passe
+        {
+          host: process.env.DB_HOST,  // Hôte
+          port: process.env.DB_PORT,  // Port
+          dialect: 'postgres',        // Type de base de données
+          logging: false,             // Désactiver les logs en production
+        }
+    );
     }
 })()
 
